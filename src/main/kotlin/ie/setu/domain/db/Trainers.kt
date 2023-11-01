ce56a4d984efd34778bc960770ae71e977c45e56
Seamus McCarthy <seamusmccarthy2508@gmail.com>
2023-11-01 23:29:03 +0000

    Add routes and handling

--- src/main/kotlin/ie/setu/domain/db/Trainers.kt
+++ src/main/kotlin/ie/setu/domain/db/Trainers.kt
@@ -2,9 +2,6 @@ package ie.setu.domain.db
 
 import org.jetbrains.exposed.sql.Table
 
-// SRP - Responsibility is to manage one activity.
-//       Database wise, this is the table object.
-
 object Trainers : Table("trainers") {
     val id = integer("id").autoIncrement().primaryKey()
     val name = varchar("name", 100)
