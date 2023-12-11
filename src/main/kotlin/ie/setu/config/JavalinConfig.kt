c7153e5fa8d40d5f0256b4a2ca1dbbd7d26c32e4
Seamus McCarthy <seamusmccarthy2508@gmail.com>
2023-12-11 22:29:24 +0000

    Complete integration tests

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -3,16 +3,30 @@ package ie.setu.config
 import ie.setu.controllers.AccountController
 import ie.setu.controllers.HealthTrackerController
 import ie.setu.controllers.StaticDataController
+import ie.setu.utils.jsonObjectMapper
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
+import io.javalin.json.JavalinJackson
+import io.javalin.vue.VueComponent
+
 
 class JavalinConfig {
 
     fun startJavalinService(): Javalin {
 
-        val app = Javalin.create().apply {
+//        val app = Javalin.create().apply {
+//            exception(Exception::class.java) { e, _ -> e.printStackTrace() }
+//            error(404) { ctx -> ctx.json("404 - Not Found") }
+//        }.start(getRemoteAssignedPort())
+
+        val app = Javalin.create{
+            //added this jsonMapper for our integration tests - serialise objects to json
+            it.jsonMapper(JavalinJackson(jsonObjectMapper()))
+            it.staticFiles.enableWebjars()
+            it.vue.vueAppName = "app" // only required for Vue 3, is defined in layout.html
+        }.apply {
             exception(Exception::class.java) { e, _ -> e.printStackTrace() }
-            error(404) { ctx -> ctx.json("404 - Not Found") }
+            error(404) { ctx -> ctx.json("404 : Not Found") }
         }.start(getRemoteAssignedPort())
 
         registerRoutes(app)
@@ -117,6 +131,14 @@ class JavalinConfig {
                     patch(StaticDataController::updateExercise)
                 }
             }
+
+            // The @routeComponent that we added in layout.html earlier will be replaced
+// by the String inside the VueComponent. This means a call to / will load
+// the layout and display our <home-page> component.
+            get("/", VueComponent("<home-page></home-page>"))
+            get("/users", VueComponent("<user-overview></user-overview>"))
+            get("/users/{user-id}", VueComponent("<user-profile></user-profile>"))
+            get("/users/{user-id}/activities", VueComponent("<user-activity-overview></user-activity-overview>"))
         }
     }
 }
